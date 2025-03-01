0ede185e6ee2b0e5d0eb14b497240d5120ee53f6
Dhyan Thakkar <thakk100@umn.edu>
2025-03-01 00:08:20 +0000

    Joystick Enabled

--- build.sh
+++ build.sh
@@ -3,8 +3,12 @@ set -e
 
 # Set the default build type
 BUILD_TYPE=RelWithDebInfo
+# export MAKEFLAGS="-j 2"
 colcon build \
         --merge-install \
         --symlink-install \
+        --executor parallel \
+        --parallel-workers 4 \
+        # --make-flags $MAKEFLAGS \
         --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
         -Wall -Wextra -Wpedantic
