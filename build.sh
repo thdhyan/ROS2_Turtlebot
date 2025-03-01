e7b8ed8cff8706f2c6791b8b251048a5f393a2c5
Dhyan Thakkar <thakk100@umn.edu>
2025-03-01 05:48:09 +0000

    correct build.sh

--- build.sh
+++ build.sh
@@ -9,6 +9,5 @@ colcon build \
         --symlink-install \
         --executor parallel \
         --parallel-workers 8 \
-        # --make-flags $MAKEFLAGS \
         --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
         -Wall -Wextra -Wpedantic
